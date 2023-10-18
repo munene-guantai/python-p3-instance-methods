44966e232cd1de7855e5092dbaafcd37b96412b1
Mikebrian <mikemunene1173@gmail.com>
2023-10-18 14:43:54 +0300

    Complete assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
+    def sit(self):
+        print("The dog is sitting.")
+
+
     pass
